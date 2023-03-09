060bd83504f43723e4344659fa8a9d55bb0bf0b0
André misuraca <m1sur4k@gmail.com>
2023-03-09 01:36:19 -0300

    Calculando media de duas colunas

--- MediaMatriz/Program.cs
+++ MediaMatriz/Program.cs
@@ -18,12 +18,14 @@
         }
         for (int i = 0; i < 5; i++)
         {
-            for (int j = 0; j < 2; j++)
+            for (int j = 0; j < 3; j++)
             {
                 Console.Write(matriz[i, j] + " ");
-                Console.Write(matriz[i,2] + " ");
+                
             }
+
             Console.WriteLine();
         }
+
     }
 }
